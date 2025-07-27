37d90d4c34518969097f75e7c2cd334167d49888
danteasc4 <zexovon@gmail.com>
2025-07-27 10:10:21 -0400

    I think this'll fix the routing properly

---
+++ docs/_next/static/chunks/app/(home)/page-2e78eedee3f01d5f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[813],{6919:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1702,23))}},e=>{e.O(0,[702,970,804,358],()=>e(e.s=6919)),_N_E=e.O()}]);
