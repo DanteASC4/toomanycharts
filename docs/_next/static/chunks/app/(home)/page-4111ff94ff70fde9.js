725c954c19b8582accfe33d1d74e9484f6021578
danteasc4 <zexovon@gmail.com>
2025-07-26 22:43:25 -0400

    Baseurl fix hopefully good now

---
+++ docs/_next/static/chunks/app/(home)/page-4111ff94ff70fde9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[813],{2570:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1702,23)),Promise.resolve().then(n.t.bind(n,3275,23))}},e=>{e.O(0,[654,970,804,358],()=>e(e.s=2570)),_N_E=e.O()}]);
